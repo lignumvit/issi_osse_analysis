d55737ed53682458b13d459f0015174fc9579d85
Christopher Kruse <ckruse@ucar.edu>
2025-04-29 09:32:46 -0600

    updated test file with different vertical levels and good data

--- test/reduce_size.bash
+++ test/reduce_size.bash
@@ -2,5 +2,6 @@
 #
 # just a simple script to save the nccopy command used to save off only the variables needed for testing
 
-rm test_dyamond.nc
-ncks -d z,40,41 issi_dyamondv2_replay_c2880_Wp_norway_20200121_0500z.nc test_dyamond.nc
+rm -f test_dyamond.nc
+ncks -d z,38,44,2 issi_dyamondv2_replay_c2880_Tp_norway_20200121_0500z.nc test_dyamond.nc
+  # note: indices after "z," are min index, max index, and stride
